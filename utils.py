3a6cac5a82dbf74c0c821b72d5abdc7d63326356
Minh Quan Huynh <hmquan1812@gmail.com>
2023-10-26 00:42:19 +1100

    Add DataLoader for FairFace

--- utils.py
+++ utils.py
@@ -4,6 +4,8 @@ import cv2
 import json
 import torch
 import torchvision
+import pandas as pd
+from PIL import Image
 
 
 class CustomDataset(torch.utils.data.dataset.Dataset):
@@ -391,3 +393,68 @@ class MultiTaskDataLoader:
 
         return data, labels, task
         
+
+
+# Add new DataLoader for FairFace Data
+class FairFaceLoader(BaseDataLoader):
+    def __init__(self, csv_file, root_dir, batch_size=128, train=True, shuffle=True, drop_last=False, transform=None):
+        """
+        Args:
+            csv_file (string): Path to the CSV file with annotations.
+            root_dir (string): Directory with all the images.
+            transform (callable, optional): Optional transform to be applied on a sample.
+        """
+        super(FairFaceLoader, self).__init__(batch_size, train, shuffle, drop_last)
+
+        self.fairface_frame = pd.read_csv(csv_file)
+        self.root_dir = root_dir
+        self.transform = transform if transform else torchvision.transforms.Compose([
+            torchvision.transforms.ToTensor(),
+            torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
+
+        # Create a dataset-like structure
+        self.dataset = CustomDataset(data=self.fairface_frame, root_dir=self.root_dir, transform=self.transform)
+
+        # You can further split your data into training and validation here if needed
+
+        self.dataloader = torch.utils.data.DataLoader(self.dataset,
+                                                      batch_size=batch_size,
+                                                      shuffle=shuffle,
+                                                      drop_last=drop_last)
+
+        self._len = len(self.fairface_frame)
+
+    def __len__(self):
+        return self._len
+
+    def __iter__(self):
+        return iter(self.dataloader)
+
+
+# The CustomDataset might need some modification to handle image loading.
+class CustomDataset(torch.utils.data.dataset.Dataset):
+    def __init__(self, data_frame, root_dir, transform=None):
+        self.annotations = data_frame
+        self.root_dir = root_dir
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.annotations)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        img_name = os.path.join(self.root_dir, self.annotations.iloc[idx, 0])  # adjust the index if necessary
+        image = Image.open(img_name)
+        
+        # Here you can add whatever information you need from the csv to the labels dictionary.
+        labels = {'age': self.annotations.iloc[idx, 1],  # adjust the index if necessary
+                  'gender': self.annotations.iloc[idx, 2],  # adjust the index if necessary
+                  'race': self.annotations.iloc[idx, 3]}  # adjust the index if necessary
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, labels
