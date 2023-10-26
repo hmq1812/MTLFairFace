88ddd0d1a1dec49b3c69f4c2bb90487e3fe914a5
Minh Quan Huynh <hmquan1812@gmail.com>
2023-10-26 23:51:19 +1100

    Debug Dataloader and Model, remove redundant code

--- models.py
+++ models.py
@@ -2,102 +2,64 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torchvision import models
+from torchvision.models import ResNet34_Weights
 
+# MTL FAIRFACE MODEL 
 
-
-class _Encoder(nn.Module):
-    def __init__(self, layers):
-        super(_Encoder, self).__init__()
-        self.layers = nn.Sequential(*layers)
-
-    def forward(self, x):
-        x = self.layers(x)
-        x = x.view(x.size(0), -1)
-
-        return x
-
-
-class _Decoder(nn.Module):
-    def __init__(self, output_size):
-        super(_Decoder, self).__init__()
-        self.layers = nn.Sequential(
-            nn.Linear(128*8*8, 512),
-            nn.BatchNorm1d(512),
-            nn.ReLU(),
-            nn.Linear(512, output_size)
-        )
-
-    def forward(self, x):
-        x = self.layers(x)
-
-        return x
-
-
-class _Model(nn.Module):
-    def __init__(self, output_size, encoder):
-        super(_Model, self).__init__()
-        self.encoder = encoder
-        self.decoder = _Decoder(output_size=output_size)
-
-    def forward(self, x):
-        x = self.encoder(x)
-        x = self.decoder(x)
-
-        return x
-
-
-def Model(num_classes, num_channels):
-    layers = [
-        nn.Conv2d(num_channels, 32, kernel_size=3, padding=1),
-        nn.BatchNorm2d(32),
-        nn.ReLU(),
-        nn.Conv2d(32, 64, kernel_size=3, padding=1, stride=2),
-        nn.BatchNorm2d(64),
-        nn.ReLU(),
-        nn.Conv2d(64, 64, kernel_size=3, padding=1),
-        nn.BatchNorm2d(64),
-        nn.ReLU(),
-        nn.Conv2d(64, 128, kernel_size=3, padding=1, stride=2),
-        nn.BatchNorm2d(128),
-        nn.ReLU(),
-        nn.Conv2d(128, 128, kernel_size=3, padding=1),
-        nn.BatchNorm2d(128),
-        nn.ReLU(),
-    ]
-
-    if isinstance(num_classes, list):
-        encoders = [_Encoder(layers=layers) for _ in num_classes]
-        return [_Model(output_size=cls, encoder=encoder) for cls, encoder in zip(num_classes, encoders)]
-    else:
-        encoder = _Encoder(layers=layers)
-        return _Model(output_size=num_classes, encoder=encoder)
-
-
-class MultiTaskModel(nn.Module):
+class MultiTaskFairFaceModel(nn.Module):
     def __init__(self, num_classes_list):
-        super(MultiTaskModel, self).__init__()
+        super(MultiTaskFairFaceModel, self).__init__()
         # Load a pre-trained ResNet-34 model
-        self.resnet = models.resnet34(pretrained=True)
-        
-        # Remove the last fully-connected layer
-        self.resnet = nn.Sequential(*list(self.resnet.children())[:-1])
-        
-        # Define new layers for multi-task learning
-        # Num_classes_list is a list of integers, each representing the number of classes for a specific task. For example: [7, 2, 5] for race, gender, and age groups respectively.
-        self.fc_layers = nn.ModuleList([nn.Linear(self.resnet[-1][-1].in_features, num_classes) for num_classes in num_classes_list])
-        
+        shared_backbone = models.resnet34(weights=ResNet34_Weights.DEFAULT)
+
+        # Remove the fully connected layer
+        modules = list(shared_backbone.children())[:-1]  # all layers except the last fully connected layer
+        self.shared_backbone = nn.Sequential(*modules)
+
+        # Find out the the fully connected layer's input features based on the backbone architecture
+        num_ftrs = shared_backbone.fc.in_features  # get the no. of in_features in fc layer
+
+        # Each task will have its own classification head
+        # Create them based on the number of tasks and their specific number of classes.
+        self.task_heads = nn.ModuleList([
+            nn.Linear(num_ftrs, num_classes) for num_classes in num_classes_list
+        ])
+
     def forward(self, x):
-        x = self.resnet(x)
-        x = torch.flatten(x, 1)  # Flatten the features
-        
-        # Number of outputs = number of tasks
-        outputs = [fc_layer(x) for fc_layer in self.fc_layers]
-        
-        return outputs  # List of outputs for different tasks
+        # Forward pass through the shared backbone
+        shared_features = self.shared_backbone(x)
+        shared_features = torch.flatten(shared_features, 1)  # Flatten the features from the backbone
+
+        # Forward pass through each task-specific head
+        logits = [task_head(shared_features) for task_head in self.task_heads]
+
+        return logits  # It returns a list of outputs for each task
+
+        # # Forward pass through the base model
+        # x = self.base_model(x)
+
+        # # Task-specific forward passes
+        # out_gender = self.fc_gender(x)
+        # out_race = self.fc_race(x)
+        # out_age = self.fc_age(x)
+
+        # # The model will output a dictionary of outputs
+        # return {
+        #     'gender': out_gender,
+        #     'race': out_race,
+        #     'age': out_age
+        # }
+
 
 
 if __name__ == "__main__":
-    # Example usage:
-    # Say we're handling three tasks (race, gender, age), and we have different numbers of classes for each
-    num_classes_list = [7, 2, 5]  # just an example, actual numbers based on the dataset
-    model = MultiTaskModel(num_classes_list=num_classes_list)
+    # Define the number of classes for each task (e.g., age, gender, race)
+    num_classes_list = [5, 2, 7]  # Just an example; adjust according to the dataset
+
+    # Create the model
+    multi_task_model = MultiTaskFairFaceModel(num_classes_list=num_classes_list)
+
+    # Move model to appropriate device (e.g., GPU if available)
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    multi_task_model = multi_task_model.to(device)
+
--- utils.py
+++ utils.py
@@ -7,18 +7,31 @@ import torchvision
 import pandas as pd
 from PIL import Image
 
-
 class CustomDataset(torch.utils.data.dataset.Dataset):
-    def __init__(self, data, labels):
-        self.data = data
-        self.labels = labels
-
-    def __getitem__(self, index):
-        return self.data[index], self.labels[index]
+    def __init__(self, data_frame, root_dir, transform=None):
+        self.annotations = data_frame
+        self.root_dir = root_dir
+        self.transform = transform
 
     def __len__(self):
-        return len(self.labels)
+        return len(self.annotations)
 
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        img_name = os.path.join(self.root_dir, self.annotations.iloc[idx, 0])  # adjust the index if necessary
+        image = Image.open(img_name)
+        
+        # Here you can add whatever information you need from the csv to the labels dictionary.
+        labels = {'age': self.annotations.iloc[idx, 1],  # adjust the index if necessary
+                  'gender': self.annotations.iloc[idx, 2],  # adjust the index if necessary
+                  'race': self.annotations.iloc[idx, 3]}  # adjust the index if necessary
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, labels
 
 class BaseDataLoader:
     def __init__(self, batch_size=1, train=True, shuffle=True, drop_last=False):
@@ -47,315 +60,6 @@ class BaseDataLoader:
     @property
     def num_classes_multi(self):
         raise NotImplementedError
-
-
-class CIFAR10Loader(BaseDataLoader):
-    def __init__(self, batch_size=128, train=True, shuffle=True, drop_last=False):
-        super(CIFAR10Loader, self).__init__(batch_size, train, shuffle, drop_last)
-        transform = torchvision.transforms.Compose(
-            [torchvision.transforms.ToTensor(),
-             torchvision.transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))]
-        )
-
-        dataset = torchvision.datasets.CIFAR10(root='./data', train=train,
-                                               download=True, transform=transform)
-        self.dataloader = torch.utils.data.DataLoader(dataset,
-                                                      batch_size=batch_size,
-                                                      shuffle=shuffle,
-                                                      drop_last=drop_last)
-        self.task_dataloader = None
-
-        self._len = 50000 if train else 10000
-        self.batch_size = batch_size
-        self.shuffle = shuffle
-        self.drop_last = drop_last
-
-
-    def _create_TaskDataLoaders(self):
-        images = []
-        labels = []
-
-        for batch_images, batch_labels in self.dataloader:
-            for i in batch_images:
-                images.append(i)
-            for l in batch_labels:
-                labels.append(l)
-
-        self.task_dataloader = []
-        for t in range(10):
-            dataset = CustomDataset(data=images.copy(), labels=[(c == t).long() for c in labels])
-            dataloader = torch.utils.data.DataLoader(dataset,
-                                                     batch_size=self.batch_size,
-                                                     shuffle=self.shuffle,
-                                                     drop_last=self.drop_last)
-            self.task_dataloader.append(dataloader)
-
-
-    def get_loader(self, loader='standard', prob='uniform'):
-        if loader == 'standard':
-            return self.dataloader
-
-        if self.task_dataloader is None:
-            self._create_TaskDataLoaders()
-
-        if loader == 'multi-task':
-            return MultiTaskDataLoader(self.task_dataloader, prob)
-        else:
-            assert loader in list(range(10)), 'Unknown loader: {}'.format(loader)
-            return self.task_dataloader[loader]
-
-
-    def get_labels(self, task='standard'):
-        if task == 'standard':
-            return list(range(10))
-        else:
-            assert task in list(range(10)), 'Unknown task: {}'.format(task)
-            labels = [0 for _ in range(10)]
-            labels[task] = 1
-            return labels
-
-
-    def __iter__(self):
-        return iter(self.dataloader)
-
-
-    def __len__(self):
-        return self._len
-
-
-    @property
-    def num_channels(self):
-        return 3
-
-
-    @property
-    def num_classes_single(self):
-        return 10
-
-
-    @property
-    def num_classes_multi(self):
-        return [2 for _ in range(10)]
-
-
-class CIFAR100Loader(BaseDataLoader):
-    def __init__(self, batch_size=128, train=True, shuffle=True, drop_last=False):
-        super(CIFAR100Loader, self).__init__(batch_size, train, shuffle, drop_last)
-        transform = torchvision.transforms.Compose(
-            [torchvision.transforms.ToTensor(),
-             torchvision.transforms.Normalize((0.5071, 0.4867, 0.4408), (0.2675, 0.2565, 0.2761))]
-        )
-
-        dataset = torchvision.datasets.CIFAR100(root='./data', train=train,
-                                               download=True, transform=transform)
-        self.dataloader = torch.utils.data.DataLoader(dataset,
-                                                      batch_size=batch_size,
-                                                      shuffle=shuffle,
-                                                      drop_last=drop_last)
-        self.task_dataloader = None
-        self.labels = None
-
-        self._len = 50000 if train else 10000
-        self.batch_size = batch_size
-        self.shuffle = shuffle
-        self.drop_last = drop_last
-
-
-    def _create_TaskDataLoaders(self):
-        with open('CIFAR100_fine2coarse.json', 'r') as f:
-            data_info = json.load(f)
-
-        images = [[] for _ in range(20)]
-        labels = [[] for _ in range(20)]
-
-        for batch_images, batch_labels in self.dataloader:
-            for i, l in zip(batch_images, batch_labels):
-                images[data_info['task'][l]].append(i)
-                labels[data_info['task'][l]].append(data_info['subclass'][l])
-
-        self.task_dataloader = []
-        for task_images, task_labels in zip(images, labels):
-            dataset = CustomDataset(data=task_images, labels=task_labels)
-            dataloader = torch.utils.data.DataLoader(dataset,
-                                                     batch_size=self.batch_size,
-                                                     shuffle=self.shuffle,
-                                                     drop_last=self.drop_last)
-            self.task_dataloader.append(dataloader)
-
-
-    def get_loader(self, loader='standard', prob='uniform'):
-        if loader == 'standard':
-            return self.dataloader
-
-        if self.task_dataloader is None:
-            self._create_TaskDataLoaders()
-
-        if loader == 'multi-task':
-            return MultiTaskDataLoader(self.task_dataloader, prob)
-        else:
-            assert loader in list(range(20)), 'Unknown loader: {}'.format(loader)
-            return self.task_dataloader[loader]
-
-
-    def _create_labels(self):
-        with open('CIFAR100_fine2coarse.json', 'r') as f:
-            data_info = json.load(f)
-
-        self.labels = [[] for _ in range(20)]
-        for i, t in enumerate(data_info['task']):
-            self.labels[t].append(i)
-
-
-    def get_labels(self, task='standard'):
-        if task == 'standard':
-            return list(range(100))
-        else:
-            assert task in list(range(20)), 'Unknown task: {}'.format(task)
-            if self.labels is None:
-                self._create_labels()
-            return self.labels[task]
-
-
-    def __iter__(self):
-        return iter(self.dataloader)
-
-
-    def __len__(self):
-        return self._len
-
-
-    @property
-    def num_channels(self):
-        return 3
-
-
-    @property
-    def num_classes_single(self):
-        return 100
-
-
-    @property
-    def num_classes_multi(self):
-        return [5 for _ in range(20)]
-
-
-class OmniglotLoader(BaseDataLoader):
-    def __init__(self, batch_size=128, train=True, shuffle=True, drop_last=False):
-        super(OmniglotLoader, self).__init__(batch_size, train, shuffle, drop_last)
-        omniglot_path = './data/omniglot'
-
-        if os.path.isdir(omniglot_path):
-            print('Files already downloaded and verified')
-        else:
-            raise FileNotFoundError('Omniglot dataset not found. Please download it and put it under \'{}\''.format(omniglot_path))
-
-        images = []
-        labels = []
-        self._len = 0
-        self.task_dataloader = []
-        self.num_classes = []
-
-        for p in [os.path.join(omniglot_path, 'images_background'), os.path.join(omniglot_path, 'images_evaluation')]:
-            for task_path in sorted(os.listdir(p)):
-                task_path = os.path.join(p, task_path)
-                task_images = []
-                task_labels = []
-                for i, cls_path in enumerate(sorted(os.listdir(task_path))):
-                    cls_path = os.path.join(task_path, cls_path)
-                    ims = [cv2.imread(os.path.join(cls_path, filename), cv2.IMREAD_GRAYSCALE) / 255 for filename in sorted(os.listdir(cls_path))]
-
-                    if train:
-                        ims = ims[:int(len(ims)*0.8)]
-                    else:
-                        ims = ims[int(len(ims)*0.8):]
-
-                    self._len += len(ims)
-                    task_images += ims
-                    task_labels += [i for _ in range(len(ims))]
-
-                task_images = np.expand_dims(task_images, 1)
-                dataset = CustomDataset(data=torch.Tensor(task_images).float(), labels=torch.Tensor(task_labels).long())
-                dataloader = torch.utils.data.DataLoader(dataset,
-                                                         batch_size=batch_size,
-                                                         shuffle=shuffle,
-                                                         drop_last=drop_last)
-                self.task_dataloader.append(dataloader)
-
-                self.num_classes.append(len(np.unique(task_labels)))
-
-                images.append(task_images)
-                labels.append(task_labels)
-
-        images = np.concatenate(images)
-        labels = np.concatenate(labels)
-
-        new_label = 0
-        new_labels = [new_label]
-        for prev_label, label in zip(labels[:-1], labels[1:]):
-            if prev_label != label:
-                new_label += 1
-            new_labels.append(new_label)
-
-        new_labels = torch.Tensor(new_labels).long()
-        images = torch.from_numpy(images).float()
-
-        dataset = CustomDataset(data=images, labels=new_labels)
-        self.dataloader = torch.utils.data.DataLoader(dataset,
-                                                      batch_size=batch_size,
-                                                      shuffle=shuffle,
-                                                      drop_last=drop_last)
-
-        self.labels = []
-        cnter = 0
-        for num_classes in self.num_classes:
-            self.labels.append(list(range(cnter, cnter + num_classes)))
-            cnter += num_classes
-
-        self.batch_size = batch_size
-        self.shuffle = shuffle
-        self.drop_last = drop_last
-
-
-    def get_loader(self, loader='standard', prob='uniform'):
-        if loader == 'standard':
-            return self.dataloader
-
-        if loader == 'multi-task':
-            return MultiTaskDataLoader(self.task_dataloader, prob)
-        else:
-            assert loader in list(range(50)), 'Unknown loader: {}'.format(loader)
-            return self.task_dataloader[loader]
-
-
-    def get_labels(self, task='standard'):
-        if task == 'standard':
-            return list(range(50))
-        else:
-            assert task in list(range(50)), 'Unknown task: {}'.format(task)
-            return self.labels[task]
-
-
-    def __iter__(self):
-        return iter(self.dataloader)
-
-
-    def __len__(self):
-        return self._len
-
-
-    @property
-    def num_channels(self):
-        return 1
-
-
-    @property
-    def num_classes_single(self):
-        return sum(self.num_classes)
-
-
-    @property
-    def num_classes_multi(self):
-        return self.num_classes
 
 
 class MultiTaskDataLoader:
@@ -414,7 +118,7 @@ class FairFaceLoader(BaseDataLoader):
         ])
 
         # Create a dataset-like structure
-        self.dataset = CustomDataset(data=self.fairface_frame, root_dir=self.root_dir, transform=self.transform)
+        self.dataset = CustomDataset(data_frame=self.fairface_frame, root_dir=self.root_dir, transform=self.transform)
 
         # You can further split your data into training and validation here if needed
 
@@ -432,29 +136,8 @@ class FairFaceLoader(BaseDataLoader):
         return iter(self.dataloader)
 
 
-# The CustomDataset might need some modification to handle image loading.
-class CustomDataset(torch.utils.data.dataset.Dataset):
-    def __init__(self, data_frame, root_dir, transform=None):
-        self.annotations = data_frame
-        self.root_dir = root_dir
-        self.transform = transform
-
-    def __len__(self):
-        return len(self.annotations)
-
-    def __getitem__(self, idx):
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
-
-        img_name = os.path.join(self.root_dir, self.annotations.iloc[idx, 0])  # adjust the index if necessary
-        image = Image.open(img_name)
-        
-        # Here you can add whatever information you need from the csv to the labels dictionary.
-        labels = {'age': self.annotations.iloc[idx, 1],  # adjust the index if necessary
-                  'gender': self.annotations.iloc[idx, 2],  # adjust the index if necessary
-                  'race': self.annotations.iloc[idx, 3]}  # adjust the index if necessary
-
-        if self.transform:
-            image = self.transform(image)
-
-        return image, labels
+if __name__ == "__main__":
+    F = FairFaceLoader("FairFaceData/fairface_label_val.csv", "FairFaceData/fairface-img-margin025-trainval/")
+    for x in F:
+        print(x)
+        break
