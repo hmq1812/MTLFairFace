78ae6056c8e8301b28af38126057233559421c3b
Minh Quan Huynh <hmquan1812@gmail.com>
2023-11-10 10:27:35 +1100

    Adjust Loss

--- loss.py
+++ loss.py
@@ -44,42 +44,45 @@ class PseudoLabelingLoss:
         self.entropy_weight = entropy_weight
     
     def compute_loss(self, outputs, labels):
-        total_loss = 0
-        task_losses = {task: 0.0 for task in self.task_names}
+            total_loss = torch.tensor(0.0, device=next(iter(outputs.values())).device)
+            task_losses = {task: 0.0 for task in self.task_names}
 
-        for task in self.task_names:
-            output = outputs[task]
-            label = labels[task]
-            
-            # Check if the label is not a special value indicating missing label
-            valid_label_mask = label != -1
-            
-            if valid_label_mask.any():
-                # Process for valid labels
-                softmax_output = F.softmax(output[valid_label_mask], dim=1)
-                max_prob, _ = torch.max(softmax_output, dim=1)
-                pseudo_label_mask = max_prob > self.threshold
-                valid_pseudo_labels = valid_label_mask & pseudo_label_mask
+            for task in self.task_names:
+                output = outputs[task]
+                label = labels[task]
+
+                # Check if the label is not a special value indicating missing label
+                valid_label_mask = label != -1
                 
-                # Compute loss for valid pseudo labels
-                if valid_pseudo_labels.any():
-                    loss = self.loss_functions[task](output[valid_pseudo_labels], label[valid_pseudo_labels])
+                if valid_label_mask.any():
+                    # Select the subset of outputs and labels that have valid labels
+                    valid_outputs = output[valid_label_mask]
+                    valid_labels = label[valid_label_mask]
+
+                    # Compute softmax and identify pseudo labels with high confidence
+                    softmax_output = F.softmax(valid_outputs, dim=1)
+                    max_prob, pseudo_labels = torch.max(softmax_output, dim=1)
+                    pseudo_label_mask = max_prob > self.threshold
+                    
+                    # Compute loss for valid pseudo labels
+                    if pseudo_label_mask.any():
+                        loss = self.loss_functions[task](valid_outputs[pseudo_label_mask], valid_labels[pseudo_label_mask])
+                    else:
+                        loss = torch.tensor(0.0, device=output.device)
+                    
+                    # Compute entropy loss for samples that are not pseudo labeled
+                    non_pseudo_label_mask = ~pseudo_label_mask
+                    if non_pseudo_label_mask.any():
+                        entropy_loss = -torch.mean(torch.sum(F.log_softmax(valid_outputs[non_pseudo_label_mask], dim=1) * softmax_output[non_pseudo_label_mask], dim=1))
+                        loss += self.entropy_weight * entropy_loss
                 else:
-                    loss = torch.tensor(0.0, device=output.device)
-                
-                # Compute entropy loss for samples that are not pseudo labeled
-                non_pseudo_labels_mask = valid_label_mask & ~pseudo_label_mask
-                if non_pseudo_labels_mask.any():
-                    entropy_loss = -torch.mean(torch.sum(F.log_softmax(output[non_pseudo_labels_mask], dim=1), dim=1))
-                    loss += self.entropy_weight * entropy_loss
-            else:
-                # Use entropy loss as a penalty for all samples if no valid labels are present
-                entropy_loss = -torch.mean(torch.sum(F.log_softmax(output, dim=1), dim=1))
-                loss = entropy_loss
-            
-            task_losses[task] = loss.item() if loss is not None else 0
-            total_loss += self.loss_weights[task] * loss
-            
-        return total_loss, task_losses
+                    # Use entropy loss as a penalty for all samples if no valid labels are present
+                    entropy_loss = -torch.mean(torch.sum(F.log_softmax(output, dim=1) * F.softmax(output, dim=1), dim=1))
+                    loss = self.entropy_weight * entropy_loss
+
+                task_losses[task] = loss.item() if loss is not None else 0
+                total_loss += self.loss_weights[task] * loss
+
+            return total_loss, task_losses
 
                 
